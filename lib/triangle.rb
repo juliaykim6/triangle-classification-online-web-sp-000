ae733e520fd9f890eb6aa5389607cc3bbb14c70e
Julia Kim <juliaykim6@gmail.com>
2021-02-19 23:56:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,6 +19,14 @@ class Triangle
      end
    end
 
+   def validate_trinagle 
+     real_triangle = [(l + w > h), (l + h > w), (w + h > l)]
+     [l, w, h].each do |side|
+       real_triangle << false if side <= 0
+       raise TriangleError if real_triangle.include?(false)
+     end
+   end
+
    class TriangleError < StandardError
    end
 
