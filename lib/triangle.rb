a1ea0f43923ca18da47731517840e80464081a94
Julia Kim <juliaykim6@gmail.com>
2021-02-19 23:52:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,15 +8,15 @@ class Triangle
      @h = height
    end
 
-   def kind 
+   def kind
      validate_triangle
      if a == b && b = c
        :equilateral
      elsif a == b || b == c || a == c
        :isosceles
-     else 
+     else
        "scalene"
-     end 
+     end
    end
 
    class TriangleError < StandardError
