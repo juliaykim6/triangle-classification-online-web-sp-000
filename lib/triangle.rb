f92eb8936f2b452dd3b357ec88697490c4ab41c2
Julia Kim <juliaykim6@gmail.com>
2021-02-20 00:02:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,9 +15,10 @@ class Triangle
        real_triangle << false if side <= 0
        raise TriangleError if real_triangle.include?(false)
      end
-   
+
 
    class TriangleError < StandardError
+   end
      def kind
        validate_triangle
        if l == w && w = h
