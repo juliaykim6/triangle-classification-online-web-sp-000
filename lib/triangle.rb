e1b80375afaeaaa6db7729f18b86458f53bc7036
Julia Kim <juliaykim6@gmail.com>
2021-02-20 00:08:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,17 @@
 class Triangle
 
-  attr_accessor :1, :2, :3
+  attr_accessor :s1, :s2, :s3
 
-    def initialize(1, 2, 3)
-      @1 = 1
-      @2 = 2
-      @3 = 3
+    def initialize(s1, s2, s3)
+      @s1 = s1
+      @s2 = s2
+      @s3 = s3
 
     end
 
     def valid_triangle?
-      real_triangle = [(1 < 2 + 3), (2 < 1 + 3), (3 < 2 + 1)]
-      [1, 2, 3].each{|side| real_triangle << false if side <= 0}
+      real_triangle = [(s1 < s2 + s3), (s2 < s1 + s3), (s3 < s2 + s1)]
+      [s1, s2, s3].each{|side| real_triangle << false if side <= 0}
       raise TriangleError if real_triangle.include?(false)
 
     end
@@ -21,9 +21,9 @@ class Triangle
 
     def kind
       valid_triangle?
-      if 1 == 2 && 1 == 3
+      if s1 == s2 && s1 == s3
         :equilateral
-      elsif 1 == 2 || 2 == 3 || 1 == 3
+      elsif s1 == s2 || s2 == s3 || s1 == s3
         :isosceles
       else
         :scalene
