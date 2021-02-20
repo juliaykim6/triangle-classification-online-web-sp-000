1baf814d2b04309401e7c6888466581800b497d2
Julia Kim <juliaykim6@gmail.com>
2021-02-20 00:04:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,33 @@
 class Triangle
 
-   attr_accessor :l, :w, :h
+  attr_accessor :side1, :side2, :side3
 
-   def initialize(l, w, h)
-     @l = l
-     @w = w
-     @h = h
-   end
+    def initialize(side1, side2, side3)
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
 
+    end
 
-   def validate_trinagle?
-     real_triangle = [(l + w > h), (l + h > w), (w + h > l)]
-     [l, w, h].each do |side|
-       real_triangle << false if side <= 0
-       raise TriangleError if real_triangle.include?(false)
-     end
+    def valid_triangle?
+      real_triangle = [(side1 < side2 + side3), (side2 < side1 + side3), (side3 < side2 + side1)]
+      [side1, side2, side3].each{|side| real_triangle << false if side <= 0}
+      raise TriangleError if real_triangle.include?(false)
 
+    end
 
-   class TriangleError < StandardError
-   end
+    class TriangleError < StandardError
+    end
 
-     def kind
-       validate_triangle
-       if l == w && w = h
-         :equilateral
-       elsif l == w || w == h || l == h
-         :isosceles
-       else
-         :scalene
-       end
-     end
-   end
+    def kind
+      valid_triangle?
+      if side1 == side2 && side1 == side3
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      else
+        :scalene
+      end
+
+    end
+  end	end
